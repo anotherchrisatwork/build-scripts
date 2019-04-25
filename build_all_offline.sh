5531bb8dc7c53e0dc1eeee2fcbcbf8e31e4343c5
chris <chris.sterritt@gd-ms.com>
2019-04-25 13:49:18 -0400

    Updated scripts to use npm instead of yarn, added build_everything.sh and its list.

--- build_all_offline.sh
+++ build_all_offline.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+rm -rf /home/chris/.cache/yarn/v4
 for i in `cat all_projects.txt` ; do
   echo "Doing $i"
   pushd $i
-  yarn install --offline > install.out 2>&1
+  npm install >& install.out
   popd
 done
---
+++ build_everything.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+rm -rf everything
+mkdir everything
+cd everything
+yarn init -y
+for i in `cat ../all.packages.txt` ; do
+  npm add $i
+done
